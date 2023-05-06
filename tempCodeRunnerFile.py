30f83f2fc41161ff43af6680137af4948038cfde
ThePillowCat <noahlevy575@gmail.com>
2023-05-06 01:33:37 -0400

    a

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+mainMenu.blit(cameraIcon, (cameraRect))
--- vision.py
+++ vision.py
@@ -4,28 +4,49 @@ import cv2
 # define a video capture object
 vid = cv2.VideoCapture(0)
 
-counter = 192
+counter = 0
+minA = 0
+maxA = 0
+errorMar = 0
+
+def th(*args):
+	global counter
+	counter = args[0]
+
+def minArea(*args):
+	global minA
+	minA = args[0]*100
+
+def maxArea(*args):
+	global maxA
+	maxA = args[0]*100
+
+def errorMargin(*args):
+	global errorMar
+	errorMar = args[0]/10
+
+cv2.namedWindow("Parameters")
+cv2.resizeWindow("Parameters", 640, 240)
+cv2.createTrackbar("Threshold", "Parameters", 0, 255, th)
+cv2.createTrackbar("minArea", "Parameters", 0, 300, minArea)
+cv2.createTrackbar("maxArea", "Parameters", 0, 300, maxArea)
+cv2.createTrackbar("marginOfError", "Parameters", 0, 10, errorMargin)
+
 
 while(True):
-	
-	# Capture the video frame
-	# by frame
+
 	ret, frame = vid.read()
-	# Display the resulting frame
-	
-	blur = cv2.GaussianBlur(frame, (5,5), 0)
-	converted = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-	# ret, thresh = cv2.threshold(converted, counter, 255, cv2.THRESH_BINARY_INV)
-	ret, thresh = cv2.threshold(converted, counter, 255, cv2.THRESH_BINARY_INV)
+	blur = cv2.GaussianBlur(frame, (7,7), 0)
+	converted = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	ret, thresh = cv2.threshold(converted, counter, 255, cv2.THRESH_BINARY)
 
 	contours, heierchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 	for i, contour in enumerate(contours):
 		if i == 0:
 			continue
-		epsilon = 0.01*cv2.arcLength(contour, True)
+		epsilon = errorMar*cv2.arcLength(contour, True)
 		approx = cv2.approxPolyDP(contour, epsilon, True)
-		# cv2.drawContours(frame,contour, 0, (255, 0, 0), 4)
 		
 		x, y, w, h = cv2.boundingRect(approx)
 		middleX = int(x - w/3)
@@ -36,25 +57,22 @@ while(True):
 		font = cv2.FONT_HERSHEY_DUPLEX
 
 		if len(approx) == 4:
-			# cv2.putText(frame, "RECTANGLE", coords, font, 1, colour, 1)
-			if (w*h) >20000 and x!= 0 and y != 0:
-				c = frame.copy()
-				cv2.rectangle(c, (int(x), int(y)), (int(x+w), int(y+h)), (0,0,255), thickness=3, lineType=cv2.LINE_8)
-				print(x, y)
+			if maxA > w * h > minA and x!= 0 and y != 0:
+				cv2.rectangle(frame, (int(x), int(y)), (int(x+w), int(y+h)), (0,0,255), thickness=10, lineType=cv2.LINE_8)
+				cv2.circle(frame, (int(x)+int(w/2), int(y)+int(h/2)), 10, (0, 255, 0), thickness=5, lineType = cv2.LINE_AA)
  
 	cv2.imshow('frame', frame)
 	cv2.imshow('thresh', thresh)
 	
-	# the 'q' button is set as the
-	# quitting button you may use any
-	# desired button of your choice
-	if cv2.waitKey(1) & 0xFF == ord('q'):
+	k = cv2.waitKey(1)
+	if k == ord('q'):
 		break
-	if cv2.waitKey(1) & 0xFF == ord('n'):
+	if k == ord('n'):
 		counter-=1
-	if cv2.waitKey(1) & 0xFF == ord('m'):
+		print(counter)
+	if k == ord('m'):
 		counter+=1
-	#qqprint(counter)
+		print(counter)
 
 # After the loop release the cap object
 vid.release()
