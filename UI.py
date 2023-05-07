b91799c4e0f056b5555c8053b2a34bc2cf475ba2
Kevin <realkevinx@gmail.com>
2023-05-07 09:04:09 -0400

    commit by iota

--- UI.py
+++ UI.py
@@ -37,61 +37,68 @@ sunnyFontM = pygame.font.Font("assets/SunnySunday.ttf", 24)
 sunnyFontL = pygame.font.Font("assets/SunnySunday.ttf", 56)
 
 # Background
-global background
+# global background
 background = pygame.image.load("assets/background2.png")
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 
 
 
-def setup():
+# def setup():
     # Background
-    global background
-    mainMenu.blit(background, (0,0))
+button1Area = mainMenu.subsurface((0, 195, 960, 100))
+button2Area = mainMenu.subsurface((0, 385, 960, 100))
+button3Area = mainMenu.subsurface((0, 575, 960, 100))
+# global background
+mainMenu.blit(background, (0,0))
 
-    global button2X, button2Y, button2W, button2H, button2Rect, button1X, button1Y, button1W, button1H, button1Rect, button3Rect, button3X, button3Y, button3W, button3H
-    # Button 1
-    button1X, button1Y = 0, 195
-    button1W, button1H = 550, 100
-    global button1Rect
-    button1Rect = pygame.Rect(button1X, button1Y, button1W, button1H)
-    # button1Img = pygame.image.load("assets/button1b.png")
-    # button1Img = pygame.transform.scale(button1Img, (button1W, button1H))
-    # mainMenu.blit(button1Img, (button1Rect))
-    pygame.draw.rect(mainMenu, (238, 225, 255), button1Rect, 50, 20)
+button1Cap = button1Area.copy()
+button2Cap = button2Area.copy()
+button3Cap = button3Area.copy()
+
+# global button2X, button2Y, button2W, button2H, button2Rect, button1X, button1Y, button1W, button1H, button1Rect, button3Rect, button3X, button3Y, button3W, button3H
+# Button 1
+button1X, button1Y = 0, 195
+button1W, button1H = 550, 100
+# global button1Rect
+button1Rect = pygame.Rect(button1X, button1Y, button1W, button1H)
+# button1Img = pygame.image.load("assets/button1b.png")
+# button1Img = pygame.transform.scale(button1Img, (button1W, button1H))
+# mainMenu.blit(button1Img, (button1Rect))
+pygame.draw.rect(mainMenu, (238, 225, 255), button1Rect, 50, 20)
 
 
 
 
-    # Button 2
-    button2X, button2Y = 410, 385
-    button2W, button2H = 550, 100
-    global button2Rect
-    button2Rect = pygame.Rect(button2X, button2Y, button2W, button2H)
-    # button2Img = pygame.image.load("assets/button3.png")
-    # button2Img = pygame.transform.scale(button2Img, (button2W, button2H))
-    # mainMenu.blit(button2Img, (button2Rect))
-    pygame.draw.rect(mainMenu, (216, 246, 255), button2Rect, 50, 20)
+# Button 2
+button2X, button2Y = 410, 385
+button2W, button2H = 550, 100
+global button2Rect
+button2Rect = pygame.Rect(button2X, button2Y, button2W, button2H)
+# button2Img = pygame.image.load("assets/button3.png")
+# button2Img = pygame.transform.scale(button2Img, (button2W, button2H))
+# mainMenu.blit(button2Img, (button2Rect))
+pygame.draw.rect(mainMenu, (216, 246, 255), button2Rect, 50, 20)
 
-    global cameraText, cameraTextRect, cameraTextX, cameraTextY
-    cameraTextX, cameraTextY = 550, 435
-    cameraText = sunnyFontL.render("CAMERA!", True, (49, 109, 234))
-    cameraTextRect = cameraText.get_rect()
-    cameraTextRect.center = (cameraTextX, cameraTextY)
-    mainMenu.blit(cameraText, cameraTextRect)
+global cameraText, cameraTextRect, cameraTextX, cameraTextY
+cameraTextX, cameraTextY = 550, 435
+cameraText = sunnyFontL.render("CAMERA!", True, (49, 109, 234))
+cameraTextRect = cameraText.get_rect()
+cameraTextRect.center = (cameraTextX, cameraTextY)
+mainMenu.blit(cameraText, cameraTextRect)
 
-    # Button 3
-    
+# Button 3
 
-    button3X, button3Y = 0, 575
-    button3W, button3H = 550, 100
 
-    button3Rect = pygame.Rect(button3X, button3Y, button3W, button3H)
-    # button3Img = pygame.image.load("assets/button2a.png")
-    # button3Img = pygame.transform.scale(button3Img, (button3W, button3H))
-    # mainMenu.blit(button3Img, (button3Rect))
-    pygame.draw.rect(mainMenu, (227, 255, 230), button3Rect, 50, 20)
+button3X, button3Y = 0, 575
+button3W, button3H = 550, 100
 
-    screen.blit(mainMenu, (0,0))
+button3Rect = pygame.Rect(button3X, button3Y, button3W, button3H)
+# button3Img = pygame.image.load("assets/button2a.png")
+# button3Img = pygame.transform.scale(button3Img, (button3W, button3H))
+# mainMenu.blit(button3Img, (button3Rect))
+pygame.draw.rect(mainMenu, (227, 255, 230), button3Rect, 50, 20)
+
+screen.blit(mainMenu, (0,0))
 
 # Camera Icon
 # cameraRect = pygame.Rect(10, HEIGHT - 110, 100, 100)
@@ -108,13 +115,10 @@ def setup():
 
 
 # Button area captures
-setup()
-button1Area = pygame.Surface((button1X, button1Y, button1W+100, button1H))
-button2Area = pygame.Surface((button2X - 100, button2Y, button2W+100, button2H))
-button3Area = pygame.Surface((button3X, button3Y, button3W+100, button3H))
-button1Cap = button1Area.copy()
-button2Cap = button2Area.copy()
-button3Cap = button3Area.copy()
+
+
+
+
 
 
 
@@ -184,8 +188,7 @@ while running:
     if selected == "menu":
         pygame.mouse.set_visible(True)
         # Background
-        if button2Hover == False:
-            setup()
+
         
 
 
@@ -211,8 +214,8 @@ while running:
 
 
         if button1Rect.collidepoint(mx, my):
-            button1Hover = True
             if mb[0] == 0:
+                button1Hover = True
                 while button1W < 600:
                     button1W += 1
                     # button1Img = pygame.transform.scale(button1Img, (button1W, button1H))
@@ -221,6 +224,35 @@ while running:
                     screen.blit(mainMenu, (0,0))
                     pygame.display.flip()
                     pygame.time.wait(2)
+        
+        if button1Hover == False:
+            mainMenu.blit(button1Cap, (0,195))
+            pygame.draw.rect(mainMenu, (238, 225, 255), (0, 195, 550, 100), 50, 20)
+
+        if button2Hover == False:
+            mainMenu.blit(button2Cap, (0, 385))
+            pygame.draw.rect(mainMenu, (216, 246, 255), (410, 385, 550, 100), 50, 20)
+        
+        if button3Hover == False:
+            mainMenu.blit(button3Cap, (0, 575))
+            pygame.draw.rect(mainMenu, (227, 255, 230), (0, 575, 550, 100), 50, 20)
+
+
+        print(button1Hover)
+        screen.blit(mainMenu, (0,0))
+
+
+        # Reset button sizes
+        button1W = 550
+        button2W = 550
+        button3W = 550
+        button2X = 410
+        button1Hover = False
+        button2Hover = False
+        button3Hover = False
+
+
+            
 
 
       
